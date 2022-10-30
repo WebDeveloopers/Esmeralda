60d1eea2f265a103d9917a86d4323eefbc6c06c9
JavierV <110652153+VMjavier@users.noreply.github.com>
2022-10-30 12:53:41 -0500

    Avance_13
    Buenos días

---
+++ AppJoyeria/frontend/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Aretes 1',
+      slug: 'arete1',
+      category: 'Aretes',
+      image: '/images/1.png',
+      price: 120000,
+      countInStock: 10,
+      brand: '101010',
+      rating: 2.6,
+      numReviews: 100,
+      description: 'Candongas en oro amarillo de 18 Kilates, Franjas',
+    },
+    {
+      name: 'Pulsera 2',
+      slug: 'pulsera2',
+      category: 'Pulseras',
+      image: '/images/2.png',
+      price: 320000,
+      countInStock: 0,
+      brand: '201010',
+      rating: 4.8,
+      numReviews: 1000,
+      description:
+        'Pulsera en oro amarillo de 18 Kilates, 20 cm. de largo, 6 mm. de ancho, con terminado en Oro',
+    },
+    {
+      name: 'Anillo 3',
+      slug: 'Anillo 3',
+      category: 'Anillos',
+      image: '/images/3.png',
+      price: 560000,
+      countInStock: 8,
+      brand: '301010',
+      rating: 4.9,
+      numReviews: 400,
+      description:
+        'Anillo en oro blanco de 18 Kilates, con zafiro central de 1.00 Ct y decoración en diamantes de 0.52 Ct',
+    },
+    {
+      name: 'Anillo 6',
+      slug: 'Anillo 6',
+      category: 'Anillos',
+      image: '/images/6.png',
+      price: 580000,
+      countInStock: 7,
+      brand: '301020',
+      rating: 4.9,
+      numReviews: 400,
+      description:
+        'Anillo en oro amarillo de 18 Kilates, con diamante central de 1.00 Ct',
+    },
+  ],
+};
+
+export default data;
